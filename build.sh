56e624fa1ef1c3edcdceb741284be0716f6b87b0
piersaj <piersaj@mat.umk.pl>
2012-08-13 10:36:00 +0200

    Fixed Build.sh script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
 
-g++ -o Neurony_PHD ./src/*.cpp ./neurony/*.cpp ./neurony/config/*.cpp ./neurony/gl_mode/*.cpp ./neurony/network/*.cpp ./neurony/statistics/*.cpp  ./neurony/statistics/spectrum/*.cpp ./neurony/statistics/path_length/*.cpp -lpthread
+g++ -o Neurony_PHD ./src/*.cpp ./neurony/*.cpp ./neurony/config/*.cpp ./neurony/gl_mode/*.cpp ./neurony/network/*.cpp ./neurony/dynamics/*.cpp ./neurony/utils/*.cpp  ./neurony/visualisation/*.cpp ./neurony/statistics/*.cpp  ./neurony/statistics/spectrum/*.cpp ./neurony/statistics/path_length/*.cpp ./neurony/statistics/degree_corelation/*.cpp -lpthread
