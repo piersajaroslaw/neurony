48124f6f33523db50c4980ffcbb0ec629349c5a1
piersaj <piersaj@o2.pl>
2017-01-12 23:38:58 +0100

    build.sh update

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/sh
+#!/bin/bash
 
-g++ -o Neurony_PHD ./src/*.cpp ./neurony/*.cpp ./neurony/config/*.cpp ./neurony/gl_mode/*.cpp ./neurony/network/*.cpp ./neurony/dynamics/*.cpp ./neurony/utils/*.cpp  ./neurony/visualisation/*.cpp ./neurony/statistics/*.cpp  ./neurony/statistics/spectrum/*.cpp ./neurony/statistics/path_length/*.cpp ./neurony/statistics/degree_corelation/*.cpp -lpthread
+g++ -O2 -o Neurony_PHD ./src/*.cpp ./neurony/*.cpp ./neurony/config/*.cpp ./neurony/gl_mode/*.cpp ./neurony/network/*.cpp ./neurony/dynamics/*.cpp ./neurony/utils/*.cpp  ./neurony/visualisation/*.cpp ./neurony/statistics/*.cpp  ./neurony/statistics/spectrum/*.cpp ./neurony/statistics/path_length/*.cpp ./neurony/statistics/degree_corelation/*.cpp -lpthread -llapack -lblas
